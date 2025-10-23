adc73dcbd461ac090c3ead41d25ada232cabe6ae
ShinigamiAo <indrahamdani003@gmail.com>
2025-10-23 10:11:11 +0700

    app.js

--- app.js
+++ app.js
@@ -1,56 +1,30 @@
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
+// Animasi sederhana huruf I LOVE YOU menggunakan mo.js
+const letters = document.querySelectorAll(".lttr");
 
-html,
-body {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #ffc568;
-  font-size: 62.5%;
-  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-}
+// Animasi masuk huruf satu per satu
+letters.forEach((lttr, i) => {
+  const delay = i * 200;
+  new mojs.Html({
+    el: lttr,
+    x: { [-100]: 0 },
+    opacity: { 0: 1 },
+    duration: 1000,
+    delay: delay,
+    easing: "elastic.out",
+  }).play();
+});
 
-.container {
-  width: 50rem;
-  height: 20rem;
-  position: relative;
-}
-
-.svg-container {
-  z-index: 2;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-
-.line {
-  fill: none;
-  stroke: #ffffff;
-  stroke-width: 8;
-  stroke-linecap: round;
-  stroke-miterlimit: 10;
-}
-
-.lttr {
-  display: inline-block;
-  color: #763c8c;
-  font-size: 8rem;
-  font-weight: 900;
-  text-transform: uppercase;
-  opacity: 0;
-}
-
-@media screen and (max-width: 520px) {
-  .lttr {
-    font-size: 4rem;
-  }
-}
+// Efek garis tengah (line animation)
+new mojs.Shape({
+  parent: ".svg-container",
+  shape: "line",
+  stroke: "white",
+  strokeWidth: 4,
+  x1: -200,
+  x2: 200,
+  y: 0,
+  strokeDasharray: "100%",
+  strokeDashoffset: { "100%": "0%" },
+  duration: 1200,
+  delay: 500,
+}).play();
